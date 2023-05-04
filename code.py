d99d811b62502f3644e09c89fcaeaa7ce4e94a74
Eric Nachtigal <Eric.Nachtigal@gmail.com>
2023-05-04 08:09:18 -0400

    Commenting and cleanup of code.py, WARNING UNTESTED

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
-# SPDX-FileCopyrightText: 2023 Eric Nachtigal
-#
-# SPDX-License-Identifier: MIT
+# CheezyPi Version 0.1 Alpha
+# 2023 Eric Nachtigal
+# MIT License
 
 import time
 import random
@@ -10,27 +10,23 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 
+#Seeding Universal Randomness (Maybe use temp sensor LSDs)
 random.seed(42)
-run = 1
 
-# The Keycode sent for each button, will be paired with a control key
+# Setting Keycodes as variables
 key_space = Keycode.SPACE
 
-# The keyboard object!
-time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
+# Creating the keyboard object
+time.sleep(1)  # Sleep to avoid a race condition on some systems
 keyboard = Keyboard(usb_hid.devices)
-keyboard_layout = KeyboardLayoutUS(keyboard)  # We're in the US :)
+keyboard_layout = KeyboardLayoutUS(keyboard)
 
-print("Pressing Spacebar at a Random intervel between 2 and 5 mins")
-
-while run:
+# Loop runs indefinately pressing spacebar every 2-5 mins with a randomly timed key
+while true:
     rdelay = random.randint(120, 300)
     rpress = random.uniform(0.01, 0.1)
-    print(rdelay)
-    print(rpress)
-    #print("Pressing Space") #This Line is for Testing Purposes
     keyboard.press(key_space)
     time.sleep(rpress)
     keyboard.release(key_space)  # ..."Release"!
     time.sleep(rdelay)
-    print("Bottom of Loop")
+    print("Loop")
